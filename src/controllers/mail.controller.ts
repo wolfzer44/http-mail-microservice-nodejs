a1ab1a1c89489b0a16ecc0e5a5a1efafca78a137
Matheus Oliveira <mattoliveirabsb@gmail.com>
2020-02-24 03:58:25 -0700

    added space between lines

--- src/controllers/mail.controller.ts
+++ src/controllers/mail.controller.ts
@@ -7,8 +7,11 @@ interface MailTypes extends Request {
 
 export const sendMail = async (req: MailTypes, res: Response) : Promise<Response> => {
   const { body: mail } = req
+
   if (!mail.from || !mail.to || !mail.subject || !mail.text) return res.status(400).json({ message: 'Missing parameters' })
+
   const response = await mailgun.send(mail)
+
   if (!response) return res.status(400).json({ message: 'Something went wrong' })
 
   return res.status(200).json(response)
